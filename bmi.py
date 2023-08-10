3c52438040a44b908a87cf8147a72a310f1952bd
terry1945 <drumgeezer@yahoo.com>
2023-08-10 17:02:30 -0600

    add lbs in to bmi

--- bmi.py
+++ bmi.py
@@ -1,6 +1,7 @@
 # BMI Calculator
 # bmi = weight/height * height (m * m) <-??
-
+# m = inches * .0354
+# k = lbs * 0.453
 
 height = input("Enter your height in m: ")
 weight = input("Enter your weight in kg: ")
@@ -8,10 +9,26 @@ weight = input("Enter your weight in kg: ")
 ht = float(height)
 wt = float(weight)
 
-print( type(ht))
+#print( type(ht))
 
 bmi = (wt) / ht **2
 
 print(round(bmi))
 
 print(int(bmi))
+
+print("\n\nNow input in American and convert to metric")
+ht_in = input("Enter your height in in: ")
+wt_lb = input("Enter your weight in lbs: ")
+ht = float(ht_in) * 0.354
+wt = float(wt_lb) * 0.453
+
+#convert to meters and kilos
+
+
+bmi = (wt) / ht **2
+
+print(bmi)
+print(round(bmi))
+
+
---
+++ namegen.py
@@ -0,0 +1,9 @@
+# see if I can do this simple first day before watching the lesson
+
+print("A Band Name Generator")
+print()
+
+name = input("Where did you grow up? ")
+pet = input("What is your pet's name? ")
+
+print("A possible band name would be ", name, pet, ".")

89536c8be938d1db3e4bdafb1166c9395d011ed5
terryandlew <leweyhorse@yahoo.com>
2023-08-11 08:11:58 -0600

    from macpro

---
+++ 2nums.py
@@ -0,0 +1,6 @@
+two_digit_number = input("Type a two digit number: ")
+
+num1 = int(two_digit_number[0])
+num2 = int(two_digit_number[1])
+#num3 = num1 + num2
+print(num1 + num2)
---
+++ ptest.py
@@ -0,0 +1,7 @@
+# print variables
+two_digit_number = input("Type a two digit number: ")
+
+num1 = int(two_digit_number[0])
+num2 = int(two_digit_number[1])
+#num3 = num1 + num2
+print(f"The two numbers added togeather are {num1 + num2}.")
